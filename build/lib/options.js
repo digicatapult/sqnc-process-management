aa3867fa5af74e1298aa00c146be20eb694702ec
Mark Addis <nitro_charlie@live.com>
2022-07-12 14:29:07 +0100

    Change docker-compose to not get latest dscp-node

--- build/lib/options.js
+++
@@ -1,8 +0,0 @@
-const option = {
-  API_HOST: 'localhost',
-  API_PORT: 9944,
-  METADATA_KEY_LENGTH: 32,
-  METADATA_VALUE_LITERAL_LENGTH: 32,
-  PROCESS_IDENTIFIER_LENGTH: 32,
-}
-export default option
