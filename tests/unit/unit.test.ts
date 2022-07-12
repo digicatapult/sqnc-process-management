dbc0d088b1435eb3d7e5b8bfca9348b524b44966
Mark Addis <nitro_charlie@live.com>
2022-07-12 10:03:34 +0100

    Unit test

---
+++ tests/unit/unit.test.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai'
+import option from '../../src/lib/options.js'
+
+describe('Option values', () => {
+
+    it('should return 9944', () => {
+        expect(option.API_PORT).to.equal(9944)
+    })
+})
