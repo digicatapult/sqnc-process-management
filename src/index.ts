8cdbd78bf0ad2f017a59ceb9bc076b954603f738
Mark Addis <nitro_charlie@live.com>
2022-07-14 10:52:27 +0100

    move getLastToken to new file

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,16 @@ import chalk from 'chalk'
 import figlet from 'figlet'
 import clear from 'clear'
 import { Command } from 'commander'
-import * as api from './lib/api.js'
+import { getLastTokenId } from './lib/lastToken.js'
 
 const program = new Command()
 
 clear()
 console.log(chalk.red(figlet.textSync('Process CLI', { horizontalLayout: 'full' })))
 
-async function getLastTokenId() {
-  await api.default.isReady
-  const lastTokenId = await api.default.query.simpleNftModule.lastToken()
-  return console.log('Last Token ID:', lastTokenId.toJSON())
-}
-
 program.action(() => {
   getLastTokenId()
 })
 
 program.parse()
+
