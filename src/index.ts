3272912280ef933f507359ae3422b3944539aec2
Paulius <paulius.michelevicius@gmail.com>
2022-11-10 11:15:15 +0000

    IN-409: a few tweaks to disable command handler.

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import chalk from 'chalk'
 import { Command } from 'commander'
 
 import { loadProcesses, disableProcess } from './lib/process/index.js'
-import version from './version.js'
+import cliVersion from './version.js'
 
 const { log } = console
 const program = new Command()
@@ -31,11 +31,17 @@ program: [
   { op: 'or' },
 ],
 }])
+
+const mapOptions = (options: Process.CLIOptions): Polkadot.Options => ({
+  API_HOST: options.host,
+  API_PORT: parseInt(options.port),
+  USER_URI: options.user,
+})
  
 program
   .name('process management')
   .description('a command line interface for managing chain processes')
-  .version(version, '-v, --version', 'output current version')
+  .version(cliVersion, '-v, --version', 'output current version')
 
 program.command('create')
   .description('A command for persisting process flows onto the chain')
@@ -43,21 +49,16 @@ program.command('create')
   .option('-h, --host <host>', 'substrate blockchain host address or FQDM, default - "localhost"', 'localhost')
   .option('-p, --port <port>', 'specify host port number if it is not a default, default - 9944', '9944')
   .option('-u, --user <user>', 'specify substrate blockhain user URI, default - "//Alice"', '//Alice')
-  .argument('<string>', `takes JSON as string example: '${example}'`)
+  .argument('<json>', `takes JSON as string example: '${example}'`)
   .action(async (data: string, options: { dryRun: boolean, port: string, user: string, host: string }) => {
     log(`
       attempting to create a process...
       options: ${b(JSON.stringify(options))}
       program: ${b(data)}
     `)
-    const { dryRun, ...rest } = options
+    const { dryRun } = options
     try {
-      const res: Process.Response = await loadProcesses({ data, dryRun, options: {
-          API_HOST: rest.host,
-          API_PORT: parseInt(rest.port),
-          USER_URI: rest.user,
-        } 
-      })
+      const res: Process.Response = await loadProcesses({ data, dryRun, options: mapOptions(options) })
       log(` ${g('command [create] executed successfully')}: ${JSON.stringify(res)}`)
       process.exit(1)
       
@@ -69,12 +70,17 @@ program.command('create')
 
 program.command('disable')
   .description('A command for disabling an existing process flows. Required process ID and version')
+  .option('--dryRun', 'to validate process and response locally before persisting on the chain, default - false')
+  .option('-h, --host <host>', 'substrate blockchain host address or FQDM, default - "localhost"', 'localhost')
+  .option('-p, --port <port>', 'specify host port number if it is not a default, default - 9944', '9944')
+  .option('-u, --user <user>', 'specify substrate blockhain user URI, default - "//Alice"', '//Alice')
   .argument('<id>', 'a valid process id that you would like to disable')
   .argument('<version>', 'a version number of a process')
-  .action(async (data: string, opt: any) => {
-    log(`attempting to disable ${b(data)}`)
+  .action(async (id: string, version: string,  options: any) => {
+    log(`attempting to disable:\nID:${b(id)}\nVersion:${b(version)}`)
     try {
-      const res: any = await disableProcess('a', 1)
+      const { dryRun } = options
+      const res: any = await disableProcess(id, parseInt(version), dryRun, mapOptions(options))
       log(` ${g('command [disable] executed successfully')}: ${JSON.stringify(res)}`)
 
       process.exit(1)
@@ -82,7 +88,7 @@ program.command('disable')
       log(`Exception has been caught: ${r(JSON.stringify(err))}`)
       program.help()
     }
-    log('not implemented', { data, opt })
+    log('not implemented', { id, version, options })
   })
 
 program.parse()
