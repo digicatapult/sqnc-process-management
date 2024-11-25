d3c6281da41fe7a3e85805ea75b9405892bd2691
Paulius Michelevicius <paulius.michelevicius@gmail.com>
2024-11-25 15:17:06 +0000

    SQNC-47: assert for type.

--- src/__tests__/version.test.ts
+++ src/__tests__/version.test.ts
@@ -5,6 +5,8 @@ import version from '../version'
 
 describe('version helper', () => {
   it('returns a version number from package.json', () => {
+    expect(typeof version).to.equal('string')
     expect(version).to.match((/^(\d+\.)?(\d+\.)?(\*|\d+)$/g))
   })
+
 })
