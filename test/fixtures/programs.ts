ad6415667e6141b05d3f833350248bf101bc11f2
Matthew Dean <matthew.dean@digicatapult.org.uk>
2025-05-13 13:44:57 +0100

    Update for sqnc-node 13

--- test/fixtures/programs.ts
+++ test/fixtures/programs.ts
@@ -1,168 +1,100 @@
 export const simple: Process.Program = [{ Restriction: 'None' }]
 
-export const simple2 = [
-  {
-    Restriction: {
-      SenderHasInputRole: {
-        index: 0,
-        role_key: 'Supplier',
-      },
-    },
-  },
-]
+export const simple2 = [{ Restriction: { SenderHasArgRole: { arg_type: 'Input', index: 0, role_key: 'Supplier' } } }]
 
 export const invalidPOSIX: Process.Program = [
   { Restriction: 'None' },
-  {
-    Restriction: {
-      SenderHasInputRole: {
-        index: 0,
-        role_key: 'Supplier',
-      },
-    },
-  },
+  { Restriction: { SenderHasArgRole: { arg_type: 'Input', index: 0, role_key: 'Supplier' } } },
   { Op: 'Or' },
-  {
-    Restriction: {
-      SenderHasOutputRole: {
-        index: 0,
-        role_key: 'Supplier',
-      },
-    },
-  },
+  { Restriction: { SenderHasArgRole: { arg_type: 'Output', index: 0, role_key: 'Supplier' } } },
 ]
 
 export const validAllRestrictions: Process.Program = [
+  { Restriction: 'Fail' },
   { Restriction: 'None' },
-  {
-    Restriction: {
-      SenderHasInputRole: {
-        index: 0,
-        role_key: 'Supplier',
-      },
-    },
-  },
   { Op: 'Or' },
+  { Restriction: 'SenderIsRoot' },
+  { Op: 'Or' },
+  { Restriction: { SenderHasArgRole: { arg_type: 'Input', index: 0, role_key: 'Supplier' } } },
+  { Op: 'Or' },
+  { Restriction: { SenderHasArgRole: { arg_type: 'Output', index: 0, role_key: 'Supplier' } } },
+  { Op: 'And' },
+  { Restriction: { ArgHasRole: { arg_type: 'Output', index: 0, role_key: 'Supplier' } } },
+  { Op: 'And' },
+  { Restriction: { ArgHasMetadata: { arg_type: 'Output', index: 0, metadata_key: 'SomeMetadataKey' } } },
+  { Op: 'And' },
+  { Restriction: { ArgHasRole: { arg_type: 'Input', index: 0, role_key: 'Supplier' } } },
+  { Op: 'And' },
+  { Restriction: { ArgHasMetadata: { arg_type: 'Input', index: 0, metadata_key: 'SomeMetadataKey' } } },
+  { Op: 'And' },
   {
     Restriction: {
-      SenderHasOutputRole: {
-        index: 0,
-        role_key: 'Supplier',
+      MatchArgsRole: {
+        left_arg_type: 'Input',
+        left_index: 0,
+        left_role_key: 'Supplier',
+        right_arg_type: 'Output',
+        right_index: 0,
+        right_role_key: 'Supplier',
       },
     },
   },
   { Op: 'And' },
   {
     Restriction: {
-      OutputHasRole: {
-        index: 0,
-        role_key: 'Supplier',
+      MatchArgsMetadataValue: {
+        left_arg_type: 'Input',
+        left_index: 0,
+        left_metadata_key: 'SomeMetadataKey',
+        right_arg_type: 'Output',
+        right_index: 0,
+        right_metadata_key: 'SomeMetadataKey',
       },
     },
   },
   { Op: 'And' },
   {
     Restriction: {
-      OutputHasMetadata: {
-        index: 0,
-        metadata_key: 'SomeMetadataKey',
+      MatchArgIdToMetadataValue: {
+        left_arg_type: 'Input',
+        left_index: 0,
+        right_arg_type: 'Output',
+        right_index: 0,
+        right_metadata_key: 'SomeMetadataKey',
       },
     },
   },
   { Op: 'And' },
-  {
-    Restriction: {
-      InputHasRole: {
-        index: 0,
-        role_key: 'Supplier',
-      },
-    },
-  },
+  { Restriction: { FixedArgCount: { arg_type: 'Input', count: 0 } } },
+  { Op: 'And' },
+  { Restriction: { FixedArgCount: { arg_type: 'Output', count: 0 } } },
   { Op: 'And' },
   {
     Restriction: {
-      InputHasMetadata: {
-        index: 0,
-        metadata_key: 'SomeMetadataKey',
-      },
-    },
-  },
-  { Op: 'And' },
-  {
-    Restriction: {
-      MatchInputOutputRole: {
-        input_index: 0,
-        input_role_key: 'Supplier',
-        output_index: 0,
-        output_role_key: 'Supplier',
-      },
-    },
-  },
-  { Op: 'And' },
-  {
-    Restriction: {
-      MatchInputOutputMetadataValue: {
-        input_index: 0,
-        input_metadata_key: 'SomeMetadataKey',
-        output_index: 0,
-        output_metadata_key: 'SomeMetadataKey',
-      },
-    },
-  },
-  { Op: 'And' },
-  {
-    Restriction: {
-      MatchInputIdOutputMetadataValue: {
-        input_index: 0,
-        output_index: 0,
-        output_metadata_key: 'SomeMetadataKey',
-      },
-    },
-  },
-  { Op: 'And' },
-  {
-    Restriction: {
-      FixedNumberOfInputs: {
-        num_inputs: 0,
-      },
-    },
-  },
-  { Op: 'And' },
-  {
-    Restriction: {
-      FixedNumberOfOutputs: {
-        num_outputs: 0,
-      },
-    },
-  },
-  { Op: 'And' },
-  {
-    Restriction: {
-      FixedInputMetadataValue: {
+      FixedArgMetadataValue: {
+        arg_type: 'Input',
         index: 0,
         metadata_key: 'SomeMetadataKey',
-        metadata_value: {
-          Literal: 'a',
-        },
+        metadata_value: { Literal: 'a' },
       },
     },
   },
   { Op: 'And' },
   {
     Restriction: {
-      FixedOutputMetadataValue: {
+      FixedArgMetadataValue: {
+        arg_type: 'Output',
         index: 0,
         metadata_key: 'SomeMetadataKey',
-        metadata_value: {
-          Literal: 'a',
-        },
+        metadata_value: { Literal: 'a' },
       },
     },
   },
   { Op: 'And' },
   {
     Restriction: {
-      FixedOutputMetadataValueType: {
+      FixedArgMetadataValueType: {
+        arg_type: 'Output',
         index: 0,
         metadata_key: 'SomeMetadataKey',
         metadata_value_type: 'Literal',
@@ -174,15 +106,7 @@ export const validAllRestrictions: Process.Program = [
 
 export const invalidRestrictionKey: Process.Program = [{ Restriction: { NotARestriction: {} } }]
 
-export const invalidRestrictionValue: Process.Program = [
-  {
-    Restriction: {
-      FixedInputMetadataValue: {
-        invalid: 0,
-      },
-    },
-  },
-]
+export const invalidRestrictionValue: Process.Program = [{ Restriction: { FixedArgMetadataValue: { invalid: 0 } } }]
 
 // WHY not to use simple/etc? why not name1 name2?
 export const multiple = (
@@ -195,29 +119,11 @@ export const multiple = (
     {
       name: process1Name,
       version: process1BumpedV,
-      program: [
-        {
-          Restriction: {
-            SenderHasInputRole: {
-              index: 0,
-              role_key: 'Supplier',
-            },
-          },
-        },
-      ],
+      program: [{ Restriction: { SenderHasArgRole: { arg_type: 'Input', index: 0, role_key: 'Supplier' } } }],
     },
     {
       name: process2Name,
       version: process2BumpedV,
-      program: [
-        {
-          Restriction: {
-            SenderHasInputRole: {
-              index: 0,
-              role_key: 'Supplier',
-            },
-          },
-        },
-      ],
+      program: [{ Restriction: { SenderHasArgRole: { arg_type: 'Input', index: 0, role_key: 'Supplier' } } }],
     },
   ])
